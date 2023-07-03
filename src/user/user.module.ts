c7eaf595d2cbd3f3385cac376e1e1baa7eb725dc
alfieqashwa <alfieqashwa@gmail.com>
2023-07-03 21:47:19 +0700

    fix: wrong room import

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,11 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AuthController } from 'src/auth/auth.controller';
-import { AuthService } from 'src/auth/auth.service';
 
-@Module({
-  controllers: [AuthController],
-  providers: [AuthService]
-})
-export class UserModule {
-
-}
+@Module({})
+export class UserModule { }
