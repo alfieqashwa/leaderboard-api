46f1cd4b65037ae48bd6d202434b6d61839017cf
alfieqashwa <alfieqashwa@gmail.com>
2023-07-03 21:24:24 +0700

    feat: setup default leaderboard and scores

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { LeaderboardModule } from './leaderboard/leaderboard.module';
+import { ScoresModule } from './scores/scores.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, LeaderboardModule, ScoresModule],
   providers: [AppService],
 })
 export class AppModule { }
