4cf4150bdaafdf2c89c68ca21835a2106730b94e
alfieqashwa <alfieqashwa@gmail.com>
2023-07-03 21:19:03 +0700

    feat: setup default auth

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,4 +1,11 @@
 import { Module } from '@nestjs/common';
+import { AuthController } from 'src/auth/auth.controller';
+import { AuthService } from 'src/auth/auth.service';
 
-@Module({})
-export class UserModule {}
+@Module({
+  controllers: [AuthController],
+  providers: [AuthService]
+})
+export class UserModule {
+
+}
