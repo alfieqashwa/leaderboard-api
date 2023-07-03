eb10b703729b9773b48d50310499f95e2256c522
alfieqashwa <alfieqashwa@gmail.com>
2023-07-03 22:34:44 +0700

    feat: setup validation pipe

--- src/auth/dto/auth.dto.ts
+++ src/auth/dto/auth.dto.ts
@@ -1,4 +1,10 @@
-export interface AuthDto {
+import { IsNotEmpty, IsString, isEmail } from "class-validator";
+
+export class AuthDto {
+  @isEmail()
+  @IsNotEmpty()
   email: string;
+  @IsString()
+  @IsNotEmpty()
   password: string;
 }
