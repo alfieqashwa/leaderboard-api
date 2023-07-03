5f58582663e1a8a9f686097696b635aea2a84b51
alfieqashwa <alfieqashwa@gmail.com>
2023-07-04 01:31:16 +0700

    feat: create get-user decorator and refactor

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import type { User } from '@prisma/client';
+import { GetUser } from 'src/auth/decorator';
+import { JwtGuard } from 'src/auth/guard';
 
-@Controller('user')
-export class UserController {}
+
+@UseGuards(JwtGuard)
+@Controller('users')
+export class UserController {
+  @Get("me")
+  getMe(@GetUser() user: User) {
+    return user
+  }
+}
