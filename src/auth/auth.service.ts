dd70a9ca0df597cc30c09400fb7400d0dcf05082
alfieqashwa <alfieqashwa@gmail.com>
2023-07-03 22:49:12 +0700

    fix: finish stup hash for fn signup

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from "@nestjs/common";
 import { AuthDto } from "./dto";
+import * as argon from "argon2"
+import { PrismaService } from "src/prisma/prisma.service";
 
 @Injectable()
 export class AuthService {
-  signup(dto: AuthDto) {
+  constructor(private prisma: PrismaService) { }
+  async signup(dto: AuthDto) {
+    try {
+
+      const hash = await argon.hash(dto.password)
+
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash
+        }
+      })
+
+      delete user.hash //avoid response the hash
+      return user
+
+    } catch (err) {
+      //TODO: not much time
+      console.error(err);
+
+    }
+
     return {
       test: "am I connected to sign up??"
     }
