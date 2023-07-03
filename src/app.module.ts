b72bfe5c822ef7cd5516c2067f6074a649d7ab08
alfieqashwa <alfieqashwa@gmail.com>
2023-07-03 22:02:04 +0700

    feat: create prisma module and provider

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,9 @@ import { UserModule } from './user/user.module';
 import { LeaderboardModule } from './leaderboard/leaderboard.module';
 import { ScoresModule } from './scores/scores.module';
 import { AuthModule } from './auth/auth.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [AuthModule, UserModule, LeaderboardModule, ScoresModule],
+  imports: [AuthModule, UserModule, LeaderboardModule, ScoresModule, PrismaModule],
 })
 export class AppModule { }
