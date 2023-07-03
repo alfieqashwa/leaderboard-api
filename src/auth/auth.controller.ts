db2f1d6fe99e05ef9e0e1ac27f59998454bbd514
alfieqashwa <alfieqashwa@gmail.com>
2023-07-03 22:40:41 +0700

    feat: also confgi dto on aut.service

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -8,13 +8,13 @@ export class AuthController {
 
   @Post('signup')
   signup(@Body() dto: AuthDto) {
-    console.log({ dto });
+    // console.log({ dto });
 
-    return this.authService.signup()
+    return this.authService.signup(dto)
   }
 
   @Post('signin')
-  signin() {
-    return this.authService.signin()
+  signin(@Body() dto: AuthDto) {
+    return this.authService.signin(dto)
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from "@nestjs/common";
+import { AuthDto } from "./dto";
 
 @Injectable()
 export class AuthService {
-  signup() {
+  signup(dto: AuthDto) {
     return {
       test: "am I connected to sign up??"
     }
   }
-  signin() {
+  signin(dto: AuthDto) {
     return {
       test: "am I connected to sign in??"
     }
