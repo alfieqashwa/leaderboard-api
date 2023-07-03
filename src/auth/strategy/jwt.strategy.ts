01dc52349bf1e0484a4b7bb417bc4213fee631ab
alfieqashwa <alfieqashwa@gmail.com>
2023-07-04 01:29:21 +0700

    fix: get payload to the user

--- src/auth/strategy/jwt.strategy.ts
+++ src/auth/strategy/jwt.strategy.ts
@@ -1,6 +1,7 @@
 import { Injectable } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
 import { PassportStrategy } from "@nestjs/passport";
+import { PrismaClient } from "@prisma/client";
 import { ExtractJwt, Strategy } from "passport-jwt";
 
 /**
@@ -8,12 +9,27 @@ import { ExtractJwt, Strategy } from "passport-jwt";
  * https://docs.nestjs.com/recipes/passport#implementing-passport-jwt
  */
 @Injectable()
-export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor(config: ConfigService) {
+export class JwtStrategy extends PassportStrategy(
+  Strategy,
+  "jwt"
+) {
+  constructor(config: ConfigService, private prisma: PrismaClient) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      // ignoreExpiration: false,
       secretOrKey: config.get("JWT_SECRET")
     })
   }
+  async validate(payload: {
+    sub: number,
+    email: string
+  }) {
+    // console.log({ payload });
+    const user = this.prisma.user.findUnique({
+      where: { id: payload.sub }
+    })
+
+    delete (await user).hash
+    return user
+  }
+
 }
