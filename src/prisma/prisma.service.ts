280d78361296c9fb7e27fcff1228886681037faf
alfieqashwa <alfieqashwa@gmail.com>
2023-07-03 22:10:41 +0700

    feat: connect db

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService {}
+export class PrismaService extends PrismaClient {
+  constructor() {
+    super({
+      datasources: {
+        db: {
+          // TODO: setup env config, that's find if it expose on git for now
+          url: "postgresql://postgres:passwd@localhost:5434/nest-leaderboard?schema=public"
+        }
+      }
+    })
+  }
+}
